391f90f6e152a5e38aa12298bae7880a018f5e3e
tamegajr <tamegajr@gmail.com>
2021-08-31 20:03:42 -0300

    5th Commit

--- shared/src/commonMain/kotlin/br/com/tamegatech/testekmm3/Greeting.kt
+++ shared/src/commonMain/kotlin/br/com/tamegatech/testekmm3/Greeting.kt
@@ -1,19 +1,19 @@
 package br.com.tamegatech.testekmm3
 
-import kotlinx.datetime.*
+//import kotlinx.datetime.*
 
 class Greeting {
     fun greeting(): String {
 //        val response: String
 //        response=""
-        return "Hello, ${Platform().platform}!" +
-        " Faltam ${daysUntilNewYear()} para o Ano Novo"
+        return "Hello, ${Platform().platform}!" //+
+//        " Faltam ${daysUntilNewYear()} para o Ano Novo"
 
     }
 
-    fun daysUntilNewYear (): Int{
-        val today = Clock.System.todayAt(TimeZone.currentSystemDefault())
-        val closestNewYear = LocalDate(today.year+1,1,1)
-        return today.daysUntil(closestNewYear)
-    }
+//    fun daysUntilNewYear (): Int{
+//        val today = Clock.System.todayAt(TimeZone.currentSystemDefault())
+//        val closestNewYear = LocalDate(today.year+1,1,1)
+//        return today.daysUntil(closestNewYear)
+//    }
 }
